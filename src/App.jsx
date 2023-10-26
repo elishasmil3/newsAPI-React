a7c5e182e507e431a554a8f35c65acaa61e9a0da
Elisha Smile <91423779+elishasmil3@users.noreply.github.com>
2023-10-26 22:44:00 +0530

    uploads

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="font-sans h-screen w-full flex justify-center items-center flex-col gap-6 text-white">
+        <h1 className="text-5xl font-black">React + Tailwind</h1>
+        <h2 className="text-4xl font-bold">Boiler Plate</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
